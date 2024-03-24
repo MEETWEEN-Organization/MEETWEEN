7da78c192265545920555829cdffb97c001ce2e8
msung99 <msung6924@naver.com>
2024-03-24 17:20:00 +0900

    backend initial settings

---
+++ backend/src/main/java/meetween/backend/MeetweenApplication.java
@@ -0,0 +1,13 @@
+package meetween.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetweenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetweenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/meetween/backend/MeetweenApplicationTests.java
@@ -0,0 +1,13 @@
+package meetween.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetweenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
