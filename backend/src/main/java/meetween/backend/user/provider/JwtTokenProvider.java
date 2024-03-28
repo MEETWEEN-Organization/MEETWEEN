c8a8e95a2bb65550032711ffb754beb5771ac480
Haon <msung6924@naver.com>
2024-03-28 18:54:57 +0900

    #4 feat: JwtTokenProvider 생성

---
+++ backend/src/main/java/meetween/backend/user/provider/JwtTokenProvider.java
@@ -0,0 +1,62 @@
+package meetween.backend.user.provider;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import meetween.backend.user.exception.InvalidTokenException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    private final SecretKey key;
+    private final long validityInMilliseconds;
+    private static final String secret_key = "qweqweoijiouqwoieoiqwjoiwjoijoqwoioijejoijodqoi";
+    private static final long expire_length = 3600;
+
+    public JwtTokenProvider() {
+        this.key = Keys.hmacShaKeyFor(secret_key.getBytes(StandardCharsets.UTF_8));
+        this.validityInMilliseconds = expire_length;
+    }
+
+    public String createToken(String payload) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(payload)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getPayload(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+            claims.getBody().getExpiration().before(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new InvalidTokenException();
+        }
+    }
+}
