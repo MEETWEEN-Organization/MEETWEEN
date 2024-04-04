55ca3b2faa5fb94aa26523b890cda5a2e748b583
soohyun <soozzang@inha.edu>
2024-04-05 03:56:06 +0900

    #9 feat: 약속 Fixture 생성

---
+++ backend/src/test/java/meetween/backend/support/fixture/AppointmentFixtures.java
@@ -0,0 +1,18 @@
+package meetween.backend.support.fixture;
+
+import meetween.backend.appointment.domain.Appointment;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class AppointmentFixtures {
+    public static Appointment 일반_약속_생성() {
+        return new Appointment(
+                "수현의 약속",
+                LocalDateTime.now().plusDays(1),
+                3L,
+                BigDecimal.valueOf(126.99597295767953),
+                BigDecimal.valueOf(37.5280674292228)
+        );
+    }
+}
