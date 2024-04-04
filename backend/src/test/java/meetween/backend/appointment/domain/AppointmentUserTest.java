b9c392e946597735b58fef97c80ab5a55881f733
soohyun <soozzang@inha.edu>
2024-04-05 03:56:45 +0900

    #9 feat: 약속-유저 중계모델 테스트 클래스 생성

---
+++ backend/src/test/java/meetween/backend/appointment/domain/AppointmentUserTest.java
@@ -0,0 +1,17 @@
+package meetween.backend.appointment.domain;
+
+import meetween.backend.support.fixture.AppointmentFixtures;
+import meetween.backend.support.fixture.UserFixtures;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AppointmentUserTest {
+
+    @DisplayName("약속과 유저의 중계모델을 생성한다")
+    @Test
+    void 약속과_유저의_중계모델을_생성한다() {
+        //when,then
+        Assertions.assertDoesNotThrow(() -> new AppointmentUser(AppointmentFixtures.일반_약속_생성(), UserFixtures.수현_유저_생성()));
+    }
+}
