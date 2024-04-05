0054e87e26363a168e7362ca2958488e937e8802
soohyun <soozzang@inha.edu>
2024-04-05 17:24:32 +0900

    #9 feat: 색상코드 추가

--- backend/src/main/java/meetween/backend/category/domain/CategoryColor.java
+++ backend/src/main/java/meetween/backend/category/domain/CategoryColor.java
@@ -1,5 +1,22 @@
 package meetween.backend.category.domain;
 
 public enum CategoryColor {
-    B4C8BB
+    _B33434("#B33434"),
+    _C47243("#C47243"),
+    _DEA12A("#DEA12A"),
+    _90B04A("#90B04A"),
+    _488BBB("#488BBB"),
+    _5B59B3("#5B59B3"),
+    _9A61D2("#9A61D2"),
+    _C161AC("#C161AC");
+
+    private final String colorCode;
+
+    private CategoryColor(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
 }
