732a3f0df02a7e4efba1ef0aad53ba297a9965f1
Haon <msung6924@naver.com>
2024-04-07 15:55:51 +0900

    #11 test: 인가 코드를 받으면 회원이 데이터베이스에 저장되는지 테스트 코드 추가

--- backend/src/test/java/meetween/backend/authentication/service/AuthServiceTest.java
+++ backend/src/test/java/meetween/backend/authentication/service/AuthServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import meetween.backend.config.TestConfig;
 import meetween.backend.authentication.dto.TokenResponse;
+import meetween.backend.user.domain.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ public class AuthServiceTest {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @DisplayName("카카오 소셜 로그인을 위한 링크를 생성한다.")
     @Test
     void 카카오_소셜_로그인을_위한_링크를_생성한다() {
@@ -25,7 +29,7 @@ public class AuthServiceTest {
         assertThat(kakaoLink).isNotEmpty();
     }
 
-    @DisplayName("토큰 생성을 하면 OAuth 서버에서 인증 후 토큰을 반환한다")
+    @DisplayName("토큰 생성을 하면 OAuth 서버에서 인증 후 토큰을 반환한다.")
     @Test
     void 토큰_생성을_하면_OAuth_서버에서_인증_후_토큰을_반환한다() {
         // given
@@ -36,5 +40,21 @@ public class AuthServiceTest {
 
         // then
         assertThat(tokenResponse.getAccessToken()).isNotEmpty();
+    }
+
+
+    @DisplayName("authorization code를 받으면 회원이 데이터베이스에 저장된다.")
+    @Test
+    void authorization_code를_받으면_회원이_데이터베이스에_저장된다() {
+        // given
+        String authorizationCode = "authorization_code";
+        authService.generateTokenWithCode(authorizationCode);
+
+        // when
+        // actual = StubOAuthClient 가 반환하는 socialLoginId
+        boolean actual = userRepository.existsBySocialLoginId("Fake Social Login Id");
+
+        // then
+        assertThat(actual).isTrue();
     }
 }
