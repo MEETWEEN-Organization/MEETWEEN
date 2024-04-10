473fdce233fbd23c9bbdd3b1e777a21b4bd2cd40
wuzoo <wndyd9911@gmail.com>
2024-04-10 15:25:04 +0900

    #26 feat: 필요한 라이브러리 설치 + ESLint, tsconfig 설정

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
