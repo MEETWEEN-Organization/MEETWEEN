912cbe40e75ead410590a397ab2b121702e0fdb2
Haon <msung6924@naver.com>
2024-04-25 17:03:01 +0900

    #25 chore: Bean 오류 방지를 위한 overriding option 활성화

--- backend/src/main/java/meetween/backend/member/domain/MemberRepository.java
+++ backend/src/main/java/meetween/backend/member/domain/MemberRepository.java
@@ -2,7 +2,6 @@ package meetween.backend.member.domain;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsBySocialLoginId(final String socialLoginId);
