9a3aecaa8898d7da0b46019b63b5c47f55420176
soohyun <soozzang@inha.edu>
2024-04-26 18:39:47 +0900

    #24 feat: UserRepository에 getById()메서드 임의 생성

--- backend/src/main/java/meetween/backend/user/domain/UserRepository.java
+++ backend/src/main/java/meetween/backend/user/domain/UserRepository.java
@@ -1,9 +1,16 @@
 package meetween.backend.user.domain;
 
 import java.util.Optional;
+
+import meetween.backend.user.exception.NoExistUserException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsBySocialLoginId(final String socialLoginId);
     Optional<User> findBySocialLoginId(final String socialLoginId);
+
+    default User getById(final Long id) {
+        return findById(id).
+                orElseThrow(NoExistUserException::new);
+    }
 }
