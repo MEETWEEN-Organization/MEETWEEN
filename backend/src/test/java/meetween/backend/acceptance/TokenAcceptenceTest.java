d29ae9a62037ccc05d0f4660480bc8fc1fd6e706
Haon <msung6924@naver.com>
2024-04-26 14:02:35 +0900

    #22 test: 카카오 Authorization URI 발급 테스트

--- backend/src/test/java/meetween/backend/acceptance/TokenAcceptenceTest.java
+++ backend/src/test/java/meetween/backend/acceptance/TokenAcceptenceTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import meetween.backend.authentication.dto.OAuthUriResponse;
 import meetween.backend.authentication.dto.TokenRequest;
 import meetween.backend.authentication.dto.TokenResponse;
 import meetween.backend.config.TestConfig;
@@ -17,6 +18,23 @@ import org.springframework.http.MediaType;
 
 @Import(TestConfig.class)
 public class TokenAcceptenceTest extends AcceptenceConfig {
+    @DisplayName("카카오 authorization uri 를 생성하고 리턴한다.")
+    @Test
+    void 카카오_authorization_uri_를_생성하고_리턴한다() {
+        // given
+        String oAuthProvider = "kakao";
+
+        // when
+        ExtractableResponse<Response> response = OAuth_인증_URI를_생성한다(oAuthProvider);
+        OAuthUriResponse oAuthUriResponse = response.as(OAuthUriResponse.class);
+
+        // then
+        assertAll(() -> {
+            상태코드_200이_반환된다(response);
+            assertThat(oAuthUriResponse.getoAuthUri()).contains("https://");
+        });
+    }
+
     @DisplayName("최초 사용자거나 기존에 존재하는 회원인 경우 200을 발급한다.")
     @Test
     void 최초_사용자거나_기존에_존재하는_회원인_경우_200을_발급한다() {
@@ -49,5 +67,14 @@ public class TokenAcceptenceTest extends AcceptenceConfig {
 
     private static void 상태코드_200이_반환된다(final ExtractableResponse<io.restassured.response.Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private ExtractableResponse<Response> OAuth_인증_URI를_생성한다(final String oauthProvider) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/auth/{provider}/link", oauthProvider)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
     }
 }
