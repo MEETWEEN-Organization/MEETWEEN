73c391e8fb09bf15a06476274b96315fa344ef13
Haon <msung6924@naver.com>
2024-04-26 17:12:06 +0900

    #32 feat: log.error 로 error 로그 출력

--- backend/src/main/java/meetween/backend/global/error/ExceptionResponse.java
+++ backend/src/main/java/meetween/backend/global/error/ExceptionResponse.java
@@ -1,2 +1,13 @@
-package meetween.backend.global.error;public class ExceptionResponse {
+package meetween.backend.global.error;
+
+public class ExceptionResponse {
+    private final String message;
+
+    public ExceptionResponse(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- backend/src/main/java/meetween/backend/global/error/GlobalExceptionHandler.java
+++ backend/src/main/java/meetween/backend/global/error/GlobalExceptionHandler.java
@@ -2,12 +2,18 @@ package meetween.backend.global.error;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ControllerAdvice {
-    private static final Logger log = LoggerFactory.getLogger(ControllerAdvice.class);
-    public void handleDefaultWException() {}
+public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    // TODO: 애플리케이션 실행시 발생하는 모든 예외 처리 Handler 구현
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionResponse> handleRuntimeException(final RuntimeException exception) {
+        log.error(exception.getMessage(), exception);
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage());
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
 }
