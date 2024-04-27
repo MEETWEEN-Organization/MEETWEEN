dc2d15dfd922178a41ace28fb9b726f2e1942156
soohyun <soozzang@inha.edu>
2024-04-27 15:30:42 +0900

    #24 refactor: 약속 service계층 테스트 중복코드 제거

--- backend/src/test/java/meetween/backend/appointment/service/AppointmentServiceTest.java
+++ backend/src/test/java/meetween/backend/appointment/service/AppointmentServiceTest.java
@@ -4,7 +4,6 @@ import meetween.backend.appointment.domain.Appointment;
 import meetween.backend.appointment.domain.AppointmentRepository;
 import meetween.backend.appointment.domain.AppointmentUser;
 import meetween.backend.appointment.domain.AppointmentUserRepository;
-import meetween.backend.appointment.dto.request.AppointmentCreateRequest;
 import meetween.backend.appointment.dto.response.AppointmentResponse;
 import meetween.backend.category.domain.Category;
 import meetween.backend.category.domain.CategoryRepository;
@@ -16,15 +15,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import static meetween.backend.support.fixture.AppointmentFixtures.일반_약속_생성;
+import static meetween.backend.support.fixture.AppointmentFixtures.수현_약속;
+import static meetween.backend.support.fixture.AppointmentFixtures.수현_약속_요청;
 import static meetween.backend.support.fixture.AppointmentUserFixtures.일반_약속_유저_생성;
-import static meetween.backend.support.fixture.CategoryFixtures.스터디_카테고리_생성;
+import static meetween.backend.support.fixture.CategoryFixtures.스터디_카테고리;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -53,8 +52,8 @@ class AppointmentServiceTest {
     private User mockUser;
 
     @BeforeEach
-    void set_up() {
-        mockUser = Mockito.spy(UserFixtures.수현_유저_생성());
+    void setUp() {
+        mockUser = Mockito.spy(UserFixtures.수현_유저());
         when(mockUser.getId()).thenReturn(1L);
     }
 
@@ -62,27 +61,17 @@ class AppointmentServiceTest {
     @Test
     void 약속을_생성한다() {
         // given
-        final AppointmentCreateRequest appointmentCreateRequest = new AppointmentCreateRequest(
-            "수현의 약속",
-                LocalDateTime.now().plusDays(1),
-                BigDecimal.valueOf(126.99597295767953),
-                BigDecimal.valueOf(37.5280674292228),
-                4L,
-                "스터디",
-                "#C161AC"
-        );
-
         given(userRepository.getById(anyLong()))
                 .willReturn(mockUser);
         given(categoryRepository.save(any(Category.class)))
-                .willReturn(스터디_카테고리_생성());
+                .willReturn(스터디_카테고리());
         given(appointmentRepository.save(any(Appointment.class)))
-                .willReturn(일반_약속_생성());
+                .willReturn(수현_약속());
         given(appointmentUserRepository.save(any(AppointmentUser.class)))
                 .willReturn(일반_약속_유저_생성());
 
         // when
-        final AppointmentResponse actual = appointmentService.save(mockUser.getId(), appointmentCreateRequest);
+        final AppointmentResponse actual = appointmentService.save(mockUser.getId(), 수현_약속_요청);
 
         // then
         Assertions.assertThat(actual.getTitle()).isEqualTo("수현의 약속");
