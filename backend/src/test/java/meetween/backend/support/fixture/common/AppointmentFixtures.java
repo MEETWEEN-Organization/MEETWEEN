a0eb57cb3a69a0cfd0b1cea7342aec6339822507
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-01 01:31:45 +0900

    #41 refactor: 약속Fixtures수정

--- backend/src/test/java/meetween/backend/support/fixture/common/AppointmentFixtures.java
+++ backend/src/test/java/meetween/backend/support/fixture/common/AppointmentFixtures.java
@@ -13,6 +13,7 @@ public class AppointmentFixtures {
 
     /* 수현 약속 */
     public static final String 수현_약속_제목 = "수현의 약속";
+    public static final Long 수현_약속_초대코드 = 123456L;
     public static final LocalDateTime 하루_뒤_시간 = LocalDateTime.now().plusDays(1);
     public static final BigDecimal 수현_약속_위도 = BigDecimal.valueOf(126.99597295767953);
     public static final BigDecimal 수현_약속_경도 = BigDecimal.valueOf(37.5280674292228);
@@ -20,7 +21,7 @@ public class AppointmentFixtures {
     public static final AppointmentResponse 수현_약속_응답 = new AppointmentResponse(수현_약속());
 
     public static Appointment 수현_약속() {
-        return new Appointment(수현_약속_제목, 하루_뒤_시간, 스터디_카테고리(), 3L, 수현_약속_위도, 수현_약속_경도);
+        return new Appointment(수현_약속_제목, 수현_약속_초대코드, 하루_뒤_시간, 스터디_카테고리(), 3L, 수현_약속_위도, 수현_약속_경도);
     }
 
     /* 민성 약속 */
