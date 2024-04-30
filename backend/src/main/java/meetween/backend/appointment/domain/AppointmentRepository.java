38f02c7842198d536084f93b88913541a92a8940
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-01 01:32:46 +0900

    #41 feat: inviteCode 관련 메서드 생성

--- backend/src/main/java/meetween/backend/appointment/domain/AppointmentRepository.java
+++ backend/src/main/java/meetween/backend/appointment/domain/AppointmentRepository.java
@@ -1,8 +1,22 @@
 package meetween.backend.appointment.domain;
 
+import meetween.backend.appointment.exception.NoExistAppointmentException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    Optional<Appointment> findByInviteCode(Long inviteCode);
+
+    boolean existsByInviteCode(Long inviteCode);
+
+    default Appointment getByInviteCode(final Long inviteCode) {
+        return findByInviteCode(inviteCode)
+                .orElseThrow(NoExistAppointmentException::new);
+    }
+
+
+
 //    List<Appointment> findByCategory(Category category);
 //    @Query("SELECT DISTINCT a FROM Appointment a " +
 //            "JOIN a.category c " +
