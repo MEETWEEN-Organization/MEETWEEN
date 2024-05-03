1351b3839e21a7b4b181928bfeafdb196d4a471c
Haon <msung6924@naver.com>
2024-05-03 16:24:33 +0900

    #27 test: 존재하지 않는 회원 정보 조회를 시도할 경우 예외를 발생하는지 테스트

--- backend/src/test/java/meetween/backend/member/controller/MemberControllerTest.java
+++ backend/src/test/java/meetween/backend/member/controller/MemberControllerTest.java
@@ -16,6 +16,7 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.requestHe
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import meetween.backend.member.exception.NoExistMemberException;
 import meetween.backend.support.annotation.ControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,13 +42,37 @@ public class MemberControllerTest extends ControllerTest {
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
                         requestHeaders(
-                                headerWithName("Authorization").description("JWT Acess Token")
+                                headerWithName("Authorization").description("JWT Access Token")
                         ),
                         responseFields(
                                 fieldWithPath("id").description("회원 ID"),
                                 fieldWithPath("socialLoginId").description("카카오 소셜 로그인 ID"),
                                 fieldWithPath("profileImageUrl").description("회원 프로필 이미지 URL"),
                                 fieldWithPath("displayName").description("회원 이름")
+                        )
+                ))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("존재하지 않는 회원 정보 조회를 시도하면 예외를 발생시킨다.")
+    @Test
+    void 존재하지_않는_회원_정보_조회를_시도하면_예외를_발생시킨다() throws Exception {
+        // given
+        given(memberService.findById(-1L)).willThrow(new NoExistMemberException());
+
+
+        // when, then
+        mockMvc.perform(get("/user/about")
+                        .header("Authorization", "Bearer DUMMY_TOKEN")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andDo(document("member/noExistMember",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("JWT Access Token")
                         )
                 ))
                 .andExpect(status().isOk());
