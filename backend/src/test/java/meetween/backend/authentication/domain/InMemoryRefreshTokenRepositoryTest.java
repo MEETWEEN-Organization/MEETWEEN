f6e9bf0afce9e3fedf3bb8e0c52ab9ab5df56a7e
Haon <msung6924@naver.com>
2024-05-04 12:20:40 +0900

    #28 test: 존재하지 않는 토큰을 조회하면 예외가 발생하는지 테스트

--- backend/src/test/java/meetween/backend/authentication/domain/InMemoryRefreshTokenRepositoryTest.java
+++ backend/src/test/java/meetween/backend/authentication/domain/InMemoryRefreshTokenRepositoryTest.java
@@ -1,8 +1,12 @@
 package meetween.backend.authentication.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import meetween.backend.appointment.domain.Appointment;
+import meetween.backend.appointment.exception.InvalidAppointmentException;
 import meetween.backend.authentication.domain.token.InMemoryRefreshTokenRepository;
+import meetween.backend.authentication.exception.NoSuchRefreshTokenException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +25,17 @@ public class InMemoryRefreshTokenRepositoryTest {
 
         // then
         assertThat(refreshTokenRepository.findById(1L)).isEqualTo(refreshToken);
+    }
+
+
+    @DisplayName("존재하지 않는 토큰을 조회하면 예외가 발생한다.")
+    @Test
+    void 존재하지_않는_토큰을_조회하면_예외가_발생한다() {
+        // given
+        long memberId = 1L;
+
+        //when, then
+        assertThatThrownBy(() -> refreshTokenRepository.findById(memberId))
+                .isInstanceOf(NoSuchRefreshTokenException.class);
     }
 }
