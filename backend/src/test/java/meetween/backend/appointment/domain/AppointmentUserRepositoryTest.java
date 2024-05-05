594cadc9de4985489161dea90d914a7b0663fca3
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-05 16:19:52 +0900

    #44 feat: 레포지토리 계층 단위 테스트 작성

---
+++ backend/src/test/java/meetween/backend/appointment/domain/AppointmentUserRepositoryTest.java
@@ -0,0 +1,54 @@
+package meetween.backend.appointment.domain;
+
+import meetween.backend.global.config.JpaAuditConfig;
+import meetween.backend.member.domain.Member;
+import meetween.backend.member.domain.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static meetween.backend.support.fixture.common.AppointmentFixtures.*;
+import static meetween.backend.support.fixture.common.MemberFixtures.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Import(JpaAuditConfig.class)
+class AppointmentUserRepositoryTest {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @Autowired
+    private AppointmentUserRepository appointmentUserRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @DisplayName("특정 유저가 속한 모든 약속-유저를 찾는다.")
+    @Test
+    void 특정_유저가_속한_모든_약속_유저를_찾는다() {
+        //given
+        Member member = new Member(수현_아이디, 수현_프로필_이미지, 수현_이름, 카카오_소셜타입);
+        Appointment appointment1 = new Appointment(수현_약속_제목, 수현_약속_초대코드, 하루_뒤_시간, 3L, 수현_약속_위도, 수현_약속_경도);
+        AppointmentUser appointmentUser1 = new AppointmentUser(appointment1, member);
+
+        Appointment appointment2 = new Appointment(민성_약속_제목, 민성_약속_초대코드, 하루_뒤_시간, 3L, 민성_약속_위도, 민성_약속_경도);
+        AppointmentUser appointmentUser2 = new AppointmentUser(appointment2, member);
+
+        memberRepository.save(member);
+        appointmentRepository.save(appointment1);
+        appointmentRepository.save(appointment2);
+        appointmentUserRepository.save(appointmentUser1);
+        appointmentUserRepository.save(appointmentUser2);
+
+        //when
+        List<AppointmentUser> actual = appointmentUserRepository.findAllByMember(member);
+
+        //then
+        assertThat(actual).hasSize(2);
+    }
+}
