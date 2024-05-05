03db43923df845f57191c0e4fac2523da27ae2c3
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-05 16:19:42 +0900

    #44 feat: 레포지토리 계층 단위 테스트 작성

--- backend/src/main/java/meetween/backend/appointment/domain/AppointmentUserRepository.java
+++ backend/src/main/java/meetween/backend/appointment/domain/AppointmentUserRepository.java
@@ -1,6 +1,11 @@
 package meetween.backend.appointment.domain;
 
+import meetween.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentUserRepository extends JpaRepository<AppointmentUser, Long> {
+    List<AppointmentUser> findAllByMember(Member member);
+
 }
