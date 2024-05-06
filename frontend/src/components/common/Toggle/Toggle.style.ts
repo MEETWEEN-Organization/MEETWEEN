2c49381830eee7f91e29ba0f1deab7a39995a890
wuzoo <wndyd9911@gmail.com>
2024-05-06 23:52:11 +0900

    feat: Toggle 컴포넌트 구현

---
+++ frontend/src/components/common/Toggle/Toggle.style.ts
@@ -0,0 +1,45 @@
+import { css } from '@emotion/react';
+
+import { Theme } from '@styles/theme/theme';
+
+export const toggleStyle = css({
+  appearance: 'none',
+
+  position: 'relative',
+  width: '60px',
+  height: '32px',
+
+  //   border: `1px solid ${Theme.color.gray200}`,
+  borderRadius: Theme.borderRadius.large,
+  backgroundColor: Theme.color.gray300,
+  boxSizing: 'content-box',
+
+  cursor: 'pointer',
+
+  '&:checked': {
+    backgroundColor: Theme.color.blue700,
+  },
+
+  '&::before': {
+    position: 'absolute',
+    left: '6px',
+    top: 0,
+    bottom: 0,
+
+    width: '20px',
+    height: '20px',
+
+    margin: 'auto 0',
+
+    borderRadius: '50%',
+    backgroundColor: Theme.color.white,
+
+    content: '""',
+
+    transition: 'all 0.1s linear',
+  },
+
+  '&:checked::before': {
+    left: '34px',
+  },
+});
