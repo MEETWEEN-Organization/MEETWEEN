08e76333d674d4e09795262b14d47288f632fb43
wuzoo <wndyd9911@gmail.com>
2024-05-10 20:49:59 +0900

    #29 feat: ColorPicker, Calendar 상수 데이터 선언

---
+++ frontend/src/constants/common.ts
@@ -0,0 +1,29 @@
+export const COLOR = {
+  /* color picker */
+  red: '#B33434',
+  orange: '#C47243',
+  yellow: '#DEA12A',
+  green: '#90B04A',
+  skyblue: '#488BBB',
+  purple200: '#5B59B3',
+  purple100: '#9A61D2',
+  pink: '#C161AC',
+} as const;
+
+export const COLOR_PALLETE = [
+  COLOR.red,
+  COLOR.yellow,
+  COLOR.skyblue,
+  COLOR.pink,
+  COLOR.orange,
+  COLOR.green,
+  COLOR.purple100,
+  COLOR.purple200,
+];
+
+export const DAY_IN_MONTHS = {
+  MIN: 35,
+  MAX: 42,
+} as const;
+
+export const DAYS_OF_WEEK = ['일', '월', '화', '수', '목', '금', '토'];
