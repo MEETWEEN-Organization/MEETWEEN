08adf350a0596e77f48f55d904312c3d3d65fc03
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-17 20:21:59 +0900

    #63 feat: 유저와 약속으로 약속-멤버를 찾는 레포지토리 메서드 생성

--- backend/src/main/java/meetween/backend/appointment/domain/AppointmentUserRepository.java
+++ backend/src/main/java/meetween/backend/appointment/domain/AppointmentUserRepository.java
@@ -1,11 +1,19 @@
 package meetween.backend.appointment.domain;
 
+import meetween.backend.appointment.exception.NoExistAppointmentUserException;
 import meetween.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AppointmentUserRepository extends JpaRepository<AppointmentUser, Long> {
     List<AppointmentUser> findAllByMember(Member member);
 
+    Optional<AppointmentUser> findByAppointmentAndMember(Appointment appointment, Member member);
+
+    default AppointmentUser getByMemberAndAppointment(final Member member, final Appointment appointment){
+        return findByAppointmentAndMember(appointment, member)
+                .orElseThrow(NoExistAppointmentUserException::new);
+    }
 }
