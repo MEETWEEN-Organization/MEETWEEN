19a3ba7fc2be2e133e1f39b6f7675b5bd5ffad92
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-17 21:07:57 +0900

    #63 test: 약속-유저 getByMemberAndAppointment레포지토리 테스트 메서드 추가

--- backend/src/test/java/meetween/backend/appointment/domain/AppointmentUserRepositoryTest.java
+++ backend/src/test/java/meetween/backend/appointment/domain/AppointmentUserRepositoryTest.java
@@ -3,7 +3,9 @@ package meetween.backend.appointment.domain;
 import static meetween.backend.support.fixture.common.AppointmentFixtures.*;
 import static meetween.backend.support.fixture.common.MemberFixtures.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import meetween.backend.appointment.exception.NoExistAppointmentUserException;
 import meetween.backend.global.config.JpaAuditConfig;
 import meetween.backend.member.domain.Member;
 import meetween.backend.member.domain.MemberRepository;
@@ -36,10 +38,10 @@ class AppointmentUserRepositoryTest {
         //given
         Member member = new Member(수현_아이디, 수현_프로필_이미지, 수현_이름, SocialType.KAKAO);
         Appointment appointment1 = new Appointment(수현_약속_제목, 수현_약속_초대코드, 하루_뒤_시간, 3L, 수현_약속_위도, 수현_약속_경도);
-        AppointmentUser appointmentUser1 = new AppointmentUser(appointment1, member);
+        AppointmentUser appointmentUser1 = new AppointmentUser(appointment1, member, MemberAuthority.ADMIN);
 
         Appointment appointment2 = new Appointment(민성_약속_제목, 민성_약속_초대코드, 하루_뒤_시간, 3L, 민성_약속_위도, 민성_약속_경도);
-        AppointmentUser appointmentUser2 = new AppointmentUser(appointment2, member);
+        AppointmentUser appointmentUser2 = new AppointmentUser(appointment2, member, MemberAuthority.ADMIN);
 
         memberRepository.save(member);
         appointmentRepository.save(appointment1);
@@ -52,5 +54,39 @@ class AppointmentUserRepositoryTest {
 
         //then
         assertThat(actual).hasSize(2);
+    }
+
+    @DisplayName("멤버와 약속을 통해 약속-유저를 찾는다.")
+    @Test
+    void 멤버와_약속을_통해_약속_유저를_찾는다() {
+        //given
+        Member member = new Member(수현_아이디, 수현_프로필_이미지, 수현_이름, SocialType.KAKAO);
+        Appointment appointment = new Appointment(수현_약속_제목, 수현_약속_초대코드, 하루_뒤_시간, 3L, 수현_약속_위도, 수현_약속_경도);
+        AppointmentUser appointmentUser = new AppointmentUser(appointment, member, MemberAuthority.ADMIN);
+
+        memberRepository.save(member);
+        appointmentRepository.save(appointment);
+        appointmentUserRepository.save(appointmentUser);
+
+        //when
+        AppointmentUser actual = appointmentUserRepository.getByMemberAndAppointment(member, appointment);
+
+        //then
+        assertThat(actual).isEqualTo(appointmentUser);
+    }
+
+    @DisplayName("입력받은 멤버와 약속을 가진 약속-유저가 없으면 예외를 발생시킨다.")
+    @Test
+    void 입력받은_멤버와_약속을_가진_약속_유저가_없으면_예외를_발생시킨다() {
+        //given
+        Member member = new Member(수현_아이디, 수현_프로필_이미지, 수현_이름, SocialType.KAKAO);
+        Appointment appointment = new Appointment(수현_약속_제목, 수현_약속_초대코드, 하루_뒤_시간, 3L, 수현_약속_위도, 수현_약속_경도);
+
+        memberRepository.save(member);
+        appointmentRepository.save(appointment);
+
+        //when, then
+        assertThatThrownBy(() -> appointmentUserRepository.getByMemberAndAppointment(member, appointment))
+                .isInstanceOf(NoExistAppointmentUserException.class);
     }
 }
