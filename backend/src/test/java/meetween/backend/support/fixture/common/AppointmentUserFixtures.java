a639d4c50ffa935eb29101b356c79732e0129fc3
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-17 21:54:31 +0900

    #63 refactor: 약속-유저 Fixtures 네이밍 변경

--- backend/src/test/java/meetween/backend/support/fixture/common/AppointmentUserFixtures.java
+++ backend/src/test/java/meetween/backend/support/fixture/common/AppointmentUserFixtures.java
@@ -5,15 +5,20 @@ import meetween.backend.appointment.domain.MemberAuthority;
 
 import static meetween.backend.support.fixture.common.AppointmentFixtures.민성_약속;
 import static meetween.backend.support.fixture.common.AppointmentFixtures.수현_약속;
+import static meetween.backend.support.fixture.common.MemberFixtures.민성_유저;
 import static meetween.backend.support.fixture.common.MemberFixtures.수현_유저;
 
 public class AppointmentUserFixtures {
 
-    public static AppointmentUser 수현_수현약속_유저_생성() {
+    public static AppointmentUser 수현약속_수현유저() {
         return new AppointmentUser(수현_약속(), 수현_유저(), MemberAuthority.ADMIN);
     }
 
-    public static AppointmentUser 수현_민성약속_유저_생성() {
+    public static AppointmentUser 수현약속_민성유저() {
+        return new AppointmentUser(수현_약속(), 민성_유저(), MemberAuthority.NORMAL);
+    }
+
+    public static AppointmentUser 민성약속_수현유저() {
         return new AppointmentUser(민성_약속(), 수현_유저(), MemberAuthority.NORMAL);
     }
 }
