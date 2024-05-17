ee90e9213d4883e7443a80d3aa4dc51479c3eb7d
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-17 20:38:24 +0900

    #63 refactor: getAuthority()로 네이밍 변경

--- backend/src/main/java/meetween/backend/appointment/domain/MemberAuthority.java
+++ backend/src/main/java/meetween/backend/appointment/domain/MemberAuthority.java
@@ -3,7 +3,7 @@ package meetween.backend.appointment.domain;
 public enum MemberAuthority {
     NORMAL, ADMIN;
 
-    public static MemberAuthority updateAuthority(MemberAuthority memberAuthority) {
+    public static MemberAuthority getAnotherAuthority(MemberAuthority memberAuthority) {
         if (memberAuthority == ADMIN) {
             return NORMAL;
         }
