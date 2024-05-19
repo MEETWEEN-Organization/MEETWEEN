43dec2fd05619ab3e520d5cb7a056b3f6207c9ea
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-19 18:03:02 +0900

    #63 feature: 레포지토리 계층 낙관적락 메서드 추가

--- backend/src/main/java/meetween/backend/appointment/domain/AppointmentUserRepository.java
+++ backend/src/main/java/meetween/backend/appointment/domain/AppointmentUserRepository.java
@@ -1,8 +1,11 @@
 package meetween.backend.appointment.domain;
 
+import jakarta.persistence.LockModeType;
 import meetween.backend.appointment.exception.NoExistAppointmentUserException;
 import meetween.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,8 +15,12 @@ public interface AppointmentUserRepository extends JpaRepository<AppointmentUser
 
     Optional<AppointmentUser> findByAppointmentAndMember(Appointment appointment, Member member);
 
-    default AppointmentUser getByMemberAndAppointment(final Member member, final Appointment appointment){
+    default AppointmentUser getByMemberAndAppointment(final Member member, final Appointment appointment) {
         return findByAppointmentAndMember(appointment, member)
                 .orElseThrow(NoExistAppointmentUserException::new);
     }
+
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("select au from AppointmentUser au where au.appointment = :appointment")
+    List<AppointmentUser> findByAppointmentWithOptimisticLock(Appointment appointment);
 }
