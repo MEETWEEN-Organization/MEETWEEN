a422123d34e2a895eea886927afd73d3f49b304d
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-27 23:14:37 +0900

    #66 feat: 장소 레포지토리 클래스 메서드 구현

--- backend/src/main/java/meetween/backend/location/domain/LocationRepository.java
+++ backend/src/main/java/meetween/backend/location/domain/LocationRepository.java
@@ -1,7 +1,21 @@
 package meetween.backend.location.domain;
 
+import meetween.backend.appointment.domain.Appointment;
+import meetween.backend.location.exception.NoExistLocationException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    List<Location> findByAppointment(Long appointmentId);
 
+    @Query("select l from Location l where l.appointment = :appointment and l.locationType = 'CHOICED'")
+    Optional<Location> findChoicedLocationByAppointment(Appointment appointment);
+
+    default Location getChoicedLocationByAppointment(Appointment appointment) {
+        return findChoicedLocationByAppointment(appointment)
+                .orElseThrow(NoExistLocationException::new);
+    }
 }
