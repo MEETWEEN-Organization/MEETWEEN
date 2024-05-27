fcdf59e3f4999bbccbd424ae030a57852eb7fdaa
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-28 00:47:39 +0900

    #66 test: 장소Fixtures생성

---
+++ backend/src/test/java/meetween/backend/support/fixture/common/LocationFixtures.java
@@ -0,0 +1,23 @@
+package meetween.backend.support.fixture.common;
+
+import meetween.backend.location.domain.Location;
+import meetween.backend.location.domain.LocationType;
+
+import java.math.BigDecimal;
+
+import static meetween.backend.support.fixture.common.AppointmentFixtures.민성_약속;
+import static meetween.backend.support.fixture.common.AppointmentFixtures.수현_약속;
+
+public class LocationFixtures {
+
+    public static final BigDecimal 인하대학교_위도 = BigDecimal.valueOf(37.450354677762);
+    public static final BigDecimal 인하대학교_경도 = BigDecimal.valueOf(126.65915614333);
+
+    public static Location 수현약속_인하대학교() {
+        return new Location(수현_약속(), 인하대학교_위도, 인하대학교_경도, LocationType.CHOICED);
+    }
+
+    public static Location 민성약속_인하대학교() {
+        return new Location(민성_약속(), 인하대학교_위도, 인하대학교_경도, LocationType.CHOICED);
+    }
+}
