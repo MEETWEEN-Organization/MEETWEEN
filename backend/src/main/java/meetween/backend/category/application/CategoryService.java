9c464db03daeaed2cd55ae821c169c5546f92cab
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-27 23:15:22 +0900

    #66 refactor: 카테고리 서비스 클래스에 장소관련 코드 주입

--- backend/src/main/java/meetween/backend/category/application/CategoryService.java
+++ backend/src/main/java/meetween/backend/category/application/CategoryService.java
@@ -1,8 +1,11 @@
 package meetween.backend.category.application;
 
+import meetween.backend.appointment.domain.Appointment;
 import meetween.backend.appointment.domain.AppointmentRepository;
 import meetween.backend.appointment.dto.response.AppointmentResponse;
 import meetween.backend.appointment.dto.response.IntegratedAppointmentResponses;
+import meetween.backend.location.domain.Location;
+import meetween.backend.location.domain.LocationRepository;
 import meetween.backend.member.domain.Member;
 import meetween.backend.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
@@ -16,16 +19,22 @@ public class CategoryService {
 
     private final AppointmentRepository appointmentRepository;
     private final MemberRepository memberRepository;
+    private final LocationRepository locationRepository;
 
-    public CategoryService(final AppointmentRepository appointmentRepository, final MemberRepository memberRepository) {
+    public CategoryService(final AppointmentRepository appointmentRepository, final MemberRepository memberRepository, final LocationRepository locationRepository) {
         this.appointmentRepository = appointmentRepository;
         this.memberRepository = memberRepository;
+        this.locationRepository = locationRepository;
     }
 
     public IntegratedAppointmentResponses findByCategory(Long memberId, String categoryName) {
         Member member = memberRepository.getById(memberId);
         return new IntegratedAppointmentResponses(appointmentRepository.findByUserAndCategoryName(member, categoryName).stream()
-                .map(AppointmentResponse::new)
+                .map(appointment -> new AppointmentResponse(appointment, getChoicedLocation(appointment)))
                 .collect(Collectors.toList()));
+    }
+
+    private Location getChoicedLocation(Appointment appointment) {
+        return locationRepository.getChoicedLocationByAppointment(appointment);
     }
 }
