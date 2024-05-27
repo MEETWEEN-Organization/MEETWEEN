3e6023ca0a723c5d08de98fa471c2b644ca862b9
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-27 23:13:48 +0900

    #66 feat: 장소 타입 enum 클래스 생성

--- backend/src/main/java/meetween/backend/location/domain/LocationType.java
+++ backend/src/main/java/meetween/backend/location/domain/LocationType.java
@@ -1,5 +1,26 @@
 package meetween.backend.location.domain;
 
+import meetween.backend.location.exception.InvalidLocationTypeException;
+
+import java.util.Objects;
+
 public enum LocationType {
-    CHOICED, PROPOSED
+    CHOICED("choiced"),
+    PROPOSED("proposed");
+
+    private final String name;
+
+    private LocationType(String name) {
+        this.name = name;
+    }
+
+    public static LocationType getLocationType(String locationType) {
+        if (Objects.equals(locationType, CHOICED.name)) {
+            return CHOICED;
+        }
+        if (Objects.equals(locationType, PROPOSED.name)) {
+            return PROPOSED;
+        }
+        throw new InvalidLocationTypeException();
+    }
 }
