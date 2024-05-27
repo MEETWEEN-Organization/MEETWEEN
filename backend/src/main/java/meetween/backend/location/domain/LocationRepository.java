d77739518626d3effce165e74413540618e47e49
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-05-27 22:30:11 +0900

    #66 feat: 장소 레포지토리 클래스 생성

---
+++ backend/src/main/java/meetween/backend/location/domain/LocationRepository.java
@@ -0,0 +1,7 @@
+package meetween.backend.location.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+}
