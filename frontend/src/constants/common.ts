426751571b37440b7c5446534346987c19e4a3d1
wuzoo <wndyd9911@gmail.com>
2024-05-30 22:51:55 +0900

    #59 refactor: 메인 페이지 상수 데이터 common으로

--- frontend/src/constants/common.ts
+++ frontend/src/constants/common.ts
@@ -27,3 +27,24 @@ export const DAY_IN_MONTHS = {
 } as const;
 
 export const DAYS_OF_WEEK = ['일', '월', '화', '수', '목', '금', '토'];
+
+export const GETTING_STARTED_TEXT = {
+  create: {
+    title: '새로운 약속 생성하기',
+    description: '친구들과 만날 중간장소를 추천받아 약속을 만들어요.',
+    buttonText: '약속 생성하기',
+    redirectURL: 'create',
+  },
+  join: {
+    title: '약속 참여하기',
+    description: '친구들이 전달한 초대코드를 입력하여 약속에 참여해요',
+    buttonText: '약속 참여하기',
+    redirectURL: 'join',
+  },
+  myInfo: {
+    title: '내 약속 확인하기',
+    description: '내 예정된 약속과 종료된 약속을 확인할 수 있어요.',
+    buttonText: '약속 확인하기',
+    redirectURL: 'info',
+  },
+} as const;
