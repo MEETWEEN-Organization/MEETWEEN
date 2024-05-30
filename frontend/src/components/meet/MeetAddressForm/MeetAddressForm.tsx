4836ac6ce7d2ab25833cd886a027dfcdc119cbf1
wuzoo <wndyd9911@gmail.com>
2024-05-30 18:12:55 +0900

    #59 feat: daum postCode 연결하여 도로명 주소 입력받고, map sdk를 통해 lat lng 정보 저장

--- frontend/src/components/meet/MeetAddressForm/MeetAddressForm.tsx
+++ frontend/src/components/meet/MeetAddressForm/MeetAddressForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Flex from '@/components/common/Flex/Flex';
 import Heading from '@/components/common/Heading/Heading';
 import Input from '@/components/common/Input/Input';
@@ -5,6 +7,8 @@ import {
   currentCompletedTextStyle,
   formStyle,
 } from '@/components/meet/MeetAddressForm/MeetAddressForm.style';
+
+import { AddressResultType, LatLngType } from '@/type/funnel';
 
 import SearchIcon from '@/assets/svg/search.svg?react';
 
@@ -16,6 +20,40 @@ interface MeetAddressFormProps {
 
 const MeetAddressForm = ({ friendsLength = 3 }: MeetAddressFormProps) => {
   const addressInputArray = new Array(friendsLength).fill(0).map((_, i) => i + 1);
+  const initialInputValues = new Array(friendsLength).fill('');
+
+  const [latLngs, setLatLngs] = useState<LatLngType[]>([]);
+  const [values, setValues] = useState<string[]>(initialInputValues);
+
+  console.log(latLngs);
+
+  const currentInputCount = values.filter((value) => !!value).length;
+
+  const handleSearchAddress = (index: number) => {
+    new window.daum.Postcode({
+      oncomplete: (data: { address: string }) => {
+        const geocoder = new window.kakao.maps.services.Geocoder();
+
+        geocoder.addressSearch(data.address, async (result: AddressResultType[], status: any) => {
+          if (status !== 'OK') return;
+
+          setLatLngs((prev) => [
+            ...prev,
+            {
+              x: result[0].x,
+              y: result[0].y,
+              key: index,
+            },
+          ]);
+          setValues((prev) => {
+            const next = prev.concat();
+            next[index] = result[0].address.address_name;
+            return next;
+          });
+        });
+      },
+    }).open();
+  };
 
   return (
     <Flex
@@ -27,7 +65,7 @@ const MeetAddressForm = ({ friendsLength = 3 }: MeetAddressFormProps) => {
       }}
     >
       <Heading size="xSmall">
-        <span css={currentCompletedTextStyle}>1&nbsp;</span>
+        <span css={currentCompletedTextStyle}>{currentInputCount}&nbsp;</span>
         <span>/&nbsp;</span>
         <span>{friendsLength}</span>
       </Heading>
@@ -37,8 +75,11 @@ const MeetAddressForm = ({ friendsLength = 3 }: MeetAddressFormProps) => {
             key={index}
             variant="text"
             icon={<SearchIcon />}
+            value={values[index - 1]}
             placeholder={`주소 ${index}을 입력해주세요`}
             onChange={() => {}}
+            onFocus={() => handleSearchAddress(index - 1)}
+            // onClick={() => handleSearchAddress(index - 1)}
           />
         ))}
       </form>
