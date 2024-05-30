91e324df4a7fb346a3488ae0181fd01cf6c8f682
wuzoo <wndyd9911@gmail.com>
2024-05-30 18:07:01 +0900

    #59 feat: title, category, categoryColor 입력 컴포넌트 생성

---
+++ frontend/src/components/meet/MeetInfoForm/MeetInfoForm.tsx
@@ -0,0 +1,40 @@
+import ColorPicker from '@/components/common/ColorPicker/ColorPicker';
+import Flex from '@/components/common/Flex/Flex';
+import Input from '@/components/common/Input/Input';
+import { formStyle } from '@/components/meet/MeetInfoForm/MeetInfoForm.style';
+
+import { COLOR_PALLETE } from '@/constants/common';
+
+interface MeetInfoFormProps {
+  selectedColor?: string;
+  onSelectColor?: (color: string) => void;
+}
+
+const MeetInfoForm = ({
+  selectedColor = COLOR_PALLETE[0],
+  onSelectColor = () => {},
+}: MeetInfoFormProps) => {
+  return (
+    <section css={formStyle}>
+      <Input
+        variant="text"
+        size="large"
+        label="약속 이름"
+        name="title"
+        placeholder="약속 이름을 설정해주세요"
+      />
+      <Flex styles={{ gap: '24px' }}>
+        <Input
+          variant="text"
+          size="large"
+          label="카테고리 이름"
+          name="category"
+          placeholder="카테고리를 설정해주세요"
+        />
+        <ColorPicker label="카테고리 색상" color={selectedColor} onSelectColor={onSelectColor} />
+      </Flex>
+    </section>
+  );
+};
+
+export default MeetInfoForm;
