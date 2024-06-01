b4a8651a9053866943000a23867a53e292eddcbf
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-06-01 17:39:49 +0900

    #77 refactor: 카테고리 서비스 메서드 파라미터 변경

--- backend/src/main/java/meetween/backend/category/application/CategoryService.java
+++ backend/src/main/java/meetween/backend/category/application/CategoryService.java
@@ -8,6 +8,7 @@ import meetween.backend.location.domain.Location;
 import meetween.backend.location.domain.LocationRepository;
 import meetween.backend.member.domain.Member;
 import meetween.backend.member.domain.MemberRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,9 +28,9 @@ public class CategoryService {
         this.locationRepository = locationRepository;
     }
 
-    public IntegratedAppointmentResponses findByCategory(Long memberId, String categoryName) {
+    public IntegratedAppointmentResponses findByCategory(Long memberId, String categoryName, PageRequest pageRequest) {
         Member member = memberRepository.getById(memberId);
-        return new IntegratedAppointmentResponses(appointmentRepository.findByUserAndCategoryName(member, categoryName).stream()
+        return new IntegratedAppointmentResponses(appointmentRepository.findByUserAndCategoryName(member, categoryName, pageRequest).stream()
                 .map(appointment -> new AppointmentResponse(appointment, getChoicedLocation(appointment)))
                 .collect(Collectors.toList()));
     }
