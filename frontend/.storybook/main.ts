fa64f2a0e52edc17b2b7b27a34213d641693bad3
wuzoo <wndyd9911@gmail.com>
2024-06-03 19:02:14 +0900

    #59 feat: 스토리북 addon-a11y 적용

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -7,8 +7,8 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-vite',
