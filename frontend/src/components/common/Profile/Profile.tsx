dff9106aaa907786a81e99c5ca8f82a35d844ce9
wuzoo <wndyd9911@gmail.com>
2024-06-17 04:19:35 +0900

    #86 refactor: size prop 추가하여 요소 크기 지정

--- frontend/src/components/common/Profile/Profile.tsx
+++ frontend/src/components/common/Profile/Profile.tsx
@@ -4,10 +4,11 @@ import { profileStyle } from '@/components/common/Profile/Profile.style';
 
 interface ProfileProps extends ComponentPropsWithoutRef<'img'> {
   imgUrl: string;
+  size?: number;
 }
 
-const Profile = ({ imgUrl, ...props }: ProfileProps) => {
-  return <img alt="프로필" css={profileStyle} src={imgUrl} {...props} />;
+const Profile = ({ size = 44, imgUrl, ...props }: ProfileProps) => {
+  return <img alt="프로필" width={size} height={size} css={profileStyle} src={imgUrl} {...props} />;
 };
 
 export default Profile;
