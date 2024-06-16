d708f469ec29f8bc33ba7d91bf43dade08f787b2
wuzoo <wndyd9911@gmail.com>
2024-06-17 04:45:21 +0900

    #86 feat: googlemaps/react-wrapper 라이브러리를 통한 래퍼 컴포넌트 생성

---
+++ frontend/src/components/common/GoogleMapWrapper/GoogleMapWrapper.tsx
@@ -0,0 +1,30 @@
+import { Status, Wrapper } from '@googlemaps/react-wrapper';
+
+import { PropsWithChildren } from 'react';
+
+import Spinner from '@/components/common/Spinner/Spinner';
+
+type GoogleMapWrapperProps = PropsWithChildren;
+
+const GoogleMapWrapper = ({ children }: GoogleMapWrapperProps) => {
+  const render = (status: Status) => {
+    console.log(status);
+    if (status === Status.FAILURE) throw new Error('google map load fail');
+    // if (status === Status.LOADING) return <Spinner />;
+
+    return <Spinner />;
+  };
+
+  return (
+    <Wrapper
+      apiKey={import.meta.env.VITE_GOOGLE_APP_KEY}
+      render={render}
+      libraries={['marker', 'places']}
+      language="ko"
+    >
+      {children}
+    </Wrapper>
+  );
+};
+
+export default GoogleMapWrapper;
