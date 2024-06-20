7d20c976ff82ba7a17ea433d71f59949369edce4
wuzoo <wndyd9911@gmail.com>
2024-06-21 06:04:48 +0900

    #86 refactor: geocoder의 위도 경도 값 x, y 반대로 도출되는 것 반영

--- frontend/src/utils/place.ts
+++ frontend/src/utils/place.ts
@@ -1,8 +1,8 @@
 import { LatLngType } from '@/type/place';
 
 export const getCalulatedMidPoint = (origins: Omit<LatLngType, 'key' | 'address_name'>[]) => {
-  const latSum = origins.reduce((acc, cur) => acc + Number(cur.x), 0);
-  const lngSum = origins.reduce((acc, cur) => acc + Number(cur.y), 0);
+  const latSum = origins.reduce((acc, cur) => acc + Number(cur.y), 0);
+  const lngSum = origins.reduce((acc, cur) => acc + Number(cur.x), 0);
 
   return {
     lat: latSum / origins.length,
