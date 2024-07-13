f2d44bed6517b8329f306b922c180e1214df5645
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-07-13 23:42:11 +0900

    #19 chore: resource 설정

---
+++ backend/src/main/java/meetween/backend/global/config/WebConfig.java
@@ -0,0 +1,15 @@
+package meetween.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("classpath:/static/", "classpath:/public/");
+    }
+}
