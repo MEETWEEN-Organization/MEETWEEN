ddceb7e11397ffaddbde4c4bad25f7a9274ce4a5
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-07-21 18:34:08 +0900

    #120 test: 데이터 소스 연결 확인

--- backend/src/test/java/meetween/backend/MeetweenApplicationTests.java
+++ backend/src/test/java/meetween/backend/MeetweenApplicationTests.java
@@ -1,12 +1,30 @@
 package meetween.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class MeetweenApplicationTests {
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
+		try (Connection connection = dataSource.getConnection()) {
+			System.out.println("Database URL: " + connection.getMetaData().getURL());
+			System.out.println("Database User: " + connection.getMetaData().getUserName());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
+//
+//	@Test
+//	void contextLoads() {
+//
+//	}
 }
