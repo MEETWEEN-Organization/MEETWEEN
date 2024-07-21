b8a9b57b211a383ee0c07d556290d4f8c514e9db
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-07-21 17:16:42 +0900

    #120 feat: 배치 업데이트용 커스텀 레포지토리 생성

---
+++ backend/src/main/java/meetween/backend/place/domain/CustomRestaurantRepository.java
@@ -0,0 +1,43 @@
+package meetween.backend.place.domain;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Component
+public class CustomRestaurantRepository {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public CustomRestaurantRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    public void saveAllRestaurantsBatch(Collection<Restaurant> restaurants) {
+        String sql = "INSERT IGNORE INTO restaurant (restaurant_id, name, address, type, latitude, longitude)" +
+                " VALUES (:restaurantId, :name, :address, :type, :latitude, :longitude, :createdAt, :updatedAt)";
+        namedParameterJdbcTemplate.batchUpdate(sql, restaurantSqlParameterSource(restaurants));
+    }
+
+    private MapSqlParameterSource[] restaurantSqlParameterSource(Collection<Restaurant> restaurants) {
+        return restaurants.stream()
+                .map(this::changeToSqlParameterSource)
+                .toArray(MapSqlParameterSource[]::new);
+    }
+
+    private MapSqlParameterSource changeToSqlParameterSource(Restaurant restaurant) {
+        LocalDateTime now = LocalDateTime.now();
+        return new MapSqlParameterSource()
+                .addValue("restaurantId", restaurant.getId())
+                .addValue("name", restaurant.getName())
+                .addValue("address", restaurant.getAdress())
+                .addValue("type", restaurant.getType())
+                .addValue("latitude", restaurant.getLatitude())
+                .addValue("longitude", restaurant.getLongitude())
+                .addValue("createdAt", now)
+                .addValue("updatedAt", now);
+    }
+}
