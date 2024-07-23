e78eecb2d2ce1a6c35845671d13919f0ab072f2f
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-07-23 23:23:29 +0900

    #126 feat: 장소 서비스 클래스 생성

---
+++ backend/src/main/java/meetween/backend/place/application/PlaceService.java
@@ -0,0 +1,38 @@
+package meetween.backend.place.application;
+
+import meetween.backend.place.domain.Coordinate;
+import meetween.backend.member.domain.MemberRepository;
+import meetween.backend.place.domain.RestaurantRepository;
+import meetween.backend.place.dto.RestaurantResponse;
+import meetween.backend.place.dto.RestaurantSpecificResponse;
+import meetween.backend.place.dto.RestaurantsByLocationRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class PlaceService {
+
+    private final RestaurantRepository restaurantRepository;
+    private final MemberRepository memberRepository;
+
+    public PlaceService(RestaurantRepository restaurantRepository, MemberRepository memberRepository) {
+        this.restaurantRepository = restaurantRepository;
+        this.memberRepository = memberRepository;
+    }
+
+    public RestaurantResponse getNearRestaurants(final RestaurantsByLocationRequest request) {
+        Coordinate coordinate = Coordinate.of(request.getLatitude(), request.getLongitude(), request.getDelta());
+
+        List<RestaurantSpecificResponse> restaurants = restaurantRepository.findAllByLatitudeAndLongitudeBetween(
+                coordinate.getMinLatitude(),
+                coordinate.getMaxLatitude(),
+                coordinate.getMinLongitude(),
+                coordinate.getMaxLongitude()
+        );
+
+        return new RestaurantResponse(restaurants);
+    }
+}
