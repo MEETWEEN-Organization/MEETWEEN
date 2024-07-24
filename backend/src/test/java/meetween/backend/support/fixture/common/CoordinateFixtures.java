ae9e29d8591214713d1cc1e83dc8a3baaef3d80a
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-07-24 13:20:22 +0900

    #128 test: Fixtures 수정

---
+++ backend/src/test/java/meetween/backend/support/fixture/common/CoordinateFixtures.java
@@ -0,0 +1,16 @@
+package meetween.backend.support.fixture.common;
+
+import meetween.backend.place.domain.Coordinate;
+import meetween.backend.place.dto.RestaurantsByLocationRequest;
+
+import java.math.BigDecimal;
+
+public class CoordinateFixtures {
+
+    public static final BigDecimal 타임스퀘어_위도 =  BigDecimal.valueOf(37.5172332566615);
+    public static final BigDecimal 타임스퀘어_경도 = BigDecimal.valueOf(126.903715993779);
+    public static final BigDecimal 위도_델타 = BigDecimal.valueOf(0.04);
+    public static final BigDecimal 경도_델타 = BigDecimal.valueOf(0.01);
+
+
+}
