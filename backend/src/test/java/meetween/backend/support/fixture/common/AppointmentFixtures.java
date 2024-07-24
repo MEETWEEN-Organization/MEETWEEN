fa6d880b650834584f77d9ea17b180a5dfe68c64
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-07-24 13:23:10 +0900

    #128 feat: 약속 Fixtures 위도 경도 스위칭

--- backend/src/test/java/meetween/backend/support/fixture/common/AppointmentFixtures.java
+++ backend/src/test/java/meetween/backend/support/fixture/common/AppointmentFixtures.java
@@ -18,8 +18,8 @@ public class AppointmentFixtures {
     public static final String 수현_약속_제목 = "수현의 약속";
     public static final InviteCode 수현_약속_초대코드 = new InviteCode(123456L);
     public static final LocalDateTime 하루_뒤_시간 = LocalDateTime.now().plusDays(1);
-    public static final BigDecimal 수현_약속_위도 = BigDecimal.valueOf(126.99597295767953);
-    public static final BigDecimal 수현_약속_경도 = BigDecimal.valueOf(37.5280674292228);
+    public static final BigDecimal 수현_약속_위도 = BigDecimal.valueOf(37.5280674292228);
+    public static final BigDecimal 수현_약속_경도 = BigDecimal.valueOf(126.99597295767953);
     public static final AppointmentCreateRequest 수현_약속_생성_요청 = new AppointmentCreateRequest(수현_약속_제목, 하루_뒤_시간, 수현_약속_위도, 수현_약속_경도, 3L, 스터디_카테고리_제목, 스터디_카테고리_컬러_문자);
     public static final AppointmentParticipateRequest 수현_약속_참여_요청 = new AppointmentParticipateRequest(수현_약속_초대코드.getCode());
     public static final AppointmentResponse 수현_약속_응답 = new AppointmentResponse(1L, 1L, 수현_약속_제목, 하루_뒤_시간, 수현_약속_위도, 수현_약속_경도, 수현_약속_초대코드.getCode(), 3L);
