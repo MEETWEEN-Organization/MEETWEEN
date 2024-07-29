793f1a9d352d2d529859b0aef1eff4fc836b273f
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-07-30 01:29:48 +0900

    #138 feat: ScrappingService 카페 로직도 추가

--- backend/src/main/java/meetween/backend/place/application/ScrappingService.java
+++ backend/src/main/java/meetween/backend/place/application/ScrappingService.java
@@ -1,8 +1,12 @@
 package meetween.backend.place.application;
 
+import meetween.backend.place.domain.Cafe;
+import meetween.backend.place.domain.CustomCafeRepository;
 import meetween.backend.place.domain.CustomRestaurantRepository;
 import meetween.backend.place.domain.Restaurant;
-import meetween.backend.place.dto.RestaurantRequest;
+import meetween.backend.place.dto.request.CafeRequest;
+import meetween.backend.place.dto.request.RestaurantRequest;
+import meetween.backend.place.infra.RestTemplateCafeRequester;
 import meetween.backend.place.infra.RestTemplateRestaurantRequester;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,10 +22,14 @@ public class ScrappingService {
     private static final int START_ITERATE_NUM = 0;
     private static final Logger log = LoggerFactory.getLogger(ScrappingService.class);
     private final RestTemplateRestaurantRequester restaurantRequester;
+    private final RestTemplateCafeRequester cafeRequester;
     private final CustomRestaurantRepository customRestaurantRepository;
+    private final CustomCafeRepository customCafeRepository;
 
-    public ScrappingService(final RestTemplateRestaurantRequester restaurantRequester, final CustomRestaurantRepository customRestaurantRepository) {
+    public ScrappingService(final RestTemplateRestaurantRequester restaurantRequester, final RestTemplateCafeRequester cafeRequester, final CustomRestaurantRepository customRestaurantRepository, final CustomCafeRepository customCafeRepository) {
         this.customRestaurantRepository = customRestaurantRepository;
+        this.customCafeRepository = customCafeRepository;
+        this.cafeRequester = cafeRequester;
         this.restaurantRequester = restaurantRequester;
     }
 
@@ -39,11 +47,14 @@ public class ScrappingService {
     }
 
     private void scrapForStartIndex(int startIndex) {
-        RestaurantRequest request = restaurantRequester.requestRestTemplate(startIndex);
+        RestaurantRequest restaurantRequest = restaurantRequester.requestRestTemplate(startIndex);
+        CafeRequest cafeRequest = cafeRequester.requestRestTemplate(startIndex);
 
-        List<Restaurant> restaurants = request.toRestaurants();
+        List<Restaurant> restaurants = restaurantRequest.toRestaurants();
+        List<Cafe> cafes = cafeRequest.toCafes();
 
         customRestaurantRepository.saveAllRestaurantsBatch(new HashSet<>(restaurants));
+        customCafeRepository.saveAllCafesBatch(new HashSet<>(cafes));
         log.info("startIndex: {}, 저장 완료", startIndex);
     }
 }
