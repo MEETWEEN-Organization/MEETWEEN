ec2d644391332c222e3f207698509ccef9f7b502
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-07-30 01:31:19 +0900

    #138 refactor: dto 디렉토리 구조 변경

--- backend/src/main/java/meetween/backend/place/domain/RestaurantRepository.java
+++ backend/src/main/java/meetween/backend/place/domain/RestaurantRepository.java
@@ -1,6 +1,5 @@
 package meetween.backend.place.domain;
 
-import meetween.backend.place.dto.RestaurantSpecificResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
