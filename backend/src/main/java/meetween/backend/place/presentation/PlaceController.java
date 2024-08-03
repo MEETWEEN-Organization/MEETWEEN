b021b95eee349dbf0a212b761a158821b79509b9
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-08-03 12:40:25 +0900

    #142 feat: 좌표 요청을 쿼리 파라미터 방식으로 변경

--- backend/src/main/java/meetween/backend/place/presentation/PlaceController.java
+++ backend/src/main/java/meetween/backend/place/presentation/PlaceController.java
@@ -1,10 +1,13 @@
 package meetween.backend.place.presentation;
 
 import meetween.backend.place.application.PlaceService;
+import meetween.backend.place.dto.request.api.RestaurantRequest;
 import meetween.backend.place.dto.response.RestaurantResponse;
 import meetween.backend.place.dto.request.PlacesByLocationRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RequestMapping("/place")
 @RestController
@@ -17,8 +20,12 @@ public class PlaceController {
     }
 
     @GetMapping("/near")
-    public ResponseEntity<RestaurantResponse> getNearRestaurants(@RequestBody final PlacesByLocationRequest request) {
-        RestaurantResponse response = placeService.getNearRestaurants(request);
+    public ResponseEntity<RestaurantResponse> getNearRestaurants(@RequestParam("latitude") BigDecimal latitude,
+                                                                 @RequestParam("longitude") BigDecimal longitude,
+                                                                 @RequestParam("latitudeDelta") BigDecimal latitudeDelta,
+                                                                 @RequestParam("longitudeDelta") BigDecimal longitudeDelta
+    ) {
+        RestaurantResponse response = placeService.getNearRestaurants(latitude, longitude, latitudeDelta, longitudeDelta);
         return ResponseEntity.ok().body(response);
     }
 
